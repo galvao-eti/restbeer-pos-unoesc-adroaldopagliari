355f27d534793e91b91bedaac5ea0a7f7ca7f206
Adroaldo Pagliari <adroaldo@outlook.com>
2016-09-07 20:42:13 -0300

    Adicionado ID para controle do zfForm

--- module/Application/src/Application/Form/Beer.php
+++ module/Application/src/Application/Form/Beer.php
@@ -2,48 +2,53 @@
 
 namespace Application\Form;
 
-use Zend\Form\Element;
 use Zend\Form\Form;
 
-class Beer extends Form
-{
-    public function __construct()
-    {
-        parent::__construct();
+class Beer extends Form {
+	public function __construct() {
+		parent::__construct();
 
-        $this->add([
-            'name' => 'name',
-            'options' => [
-                'label' => 'Beer name',
-            ],
-            'type'  => 'Text',
-        ]);
-        $this->add([
-            'name' => 'style',
-            'options' => [
-                'label' => 'Beer style',
-            ],
-            'type'  => 'Text',
-        ]);
+		$this->add([
+			'name' => 'id',
+			'attributes' => [
+				'type' => 'hidden',
+			],
+		]);
 
-         $this->add([
-            'name' => 'img',
-            'options' => [
-                'label' => 'Beer image',
-            ],
-            'type'  => 'Text',
-        ]);
+		$this->add([
+			'name' => 'name',
+			'options' => [
+				'label' => 'Beer name',
+			],
+			'type' => 'Text',
+		]);
 
-        $this->add([
-            'name' => 'send',
-            'type'  => 'Submit',
-            'attributes' => [
-                'value' => 'Submit',
-            ],
-        ]);
+		$this->add([
+			'name' => 'style',
+			'options' => [
+				'label' => 'Beer style',
+			],
+			'type' => 'Text',
+		]);
 
-        $this->setAttribute('action', '/save');
-        $this->setAttribute('method', 'post');
-    }
+		$this->add([
+			'name' => 'img',
+			'options' => [
+				'label' => 'Beer image',
+			],
+			'type' => 'Text',
+		]);
+
+		$this->add([
+			'name' => 'send',
+			'type' => 'Submit',
+			'attributes' => [
+				'value' => 'Submit',
+			],
+		]);
+
+		$this->setAttribute('action', '/save');
+		$this->setAttribute('method', 'post');
+	}
 
 }
