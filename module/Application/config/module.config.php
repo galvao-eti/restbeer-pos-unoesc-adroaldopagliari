c73c2ae615fbc1df9df31e504ee2c66c46c357f8
Adroaldo Pagliari <adroaldo@outlook.com>
2016-09-09 10:49:10 -0300

    Adicionada rota para edição e view da deleção

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -13,30 +13,33 @@ return array(
             'home' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' => array(
-                    'route' => '/',
+                    'route'    => '/',
                     'defaults' => array(
                         'controller' => 'Application\Controller\Index',
-                        'action' => 'index',
-                    ),
-                ),
-            ),
-            'create' => array(
-                'type' => 'Zend\Mvc\Router\Http\Literal',
-                'options' => array(
-                    'route' => '/create',
-                    'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
-                        'action' => 'create',
+                        'action'     => 'index',
                     ),
                 ),
             ),
             'insert' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' => array(
-                    'route' => '/insert',
+                    'route'    => '/insert',
                     'defaults' => array(
                         'controller' => 'Application\Controller\Index',
-                        'action' => 'insert',
+                        'action'     => 'insert',
+                    ),
+                ),
+            ),
+            'edit' => array(
+                'type' => 'Zend\Mvc\Router\Http\Segment',
+                'options' => array(
+                    'route'    => '/edit/[:id]',
+                    'constraints' => array(
+                        'id' => '[0-9]+'
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Application\Controller\Index',
+                        'action'     => 'insert',
                     ),
                 ),
             ),
@@ -44,6 +47,9 @@ return array(
                 'type' => 'Zend\Mvc\Router\Http\Segment',
                 'options' => array(
                     'route'    => '/delete/[:id]',
+                    'constraints' => array(
+                        'id' => '[0-9]+'
+                    ),
                     'defaults' => array(
                         'controller' => 'Application\Controller\Index',
                         'action'     => 'delete',
@@ -55,24 +61,24 @@ return array(
             // module. Simply drop new controllers in, and you can access them
             // using the path /application/:controller/:action
             'application' => array(
-                'type' => 'Literal',
+                'type'    => 'Literal',
                 'options' => array(
-                    'route' => '/application',
+                    'route'    => '/application',
                     'defaults' => array(
                         '__NAMESPACE__' => 'Application\Controller',
-                        'controller' => 'Index',
-                        'action' => 'index',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
                     ),
                 ),
                 'may_terminate' => true,
                 'child_routes' => array(
                     'default' => array(
-                        'type' => 'Segment',
+                        'type'    => 'Segment',
                         'options' => array(
-                            'route' => '/[:controller[/:action]]',
+                            'route'    => '/[:controller[/:action]]',
                             'constraints' => array(
                                 'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
                             ),
                             'defaults' => array(
                             ),
@@ -95,42 +101,42 @@ return array(
         ),
         'factories' => array(
             'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
-            'Application\Model\BeerTableGateway' => function ($sm) {
-                $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
-                $resultSetPrototype = new Zend\Db\ResultSet\ResultSet();
-                $resultSetPrototype->setArrayObjectPrototype(new Application\Model\Beer());
-                $tableGateway = new Zend\Db\TableGateway\TableGateway('beer', $dbAdapter, null, $resultSetPrototype);
-                $beerTableGateway = new Application\Model\BeerTableGateway($tableGateway);
-                return $beerTableGateway;
-            },
-        ),
+            'Application\Model\BeerTableGateway' =>  function($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new Zend\Db\ResultSet\ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Application\Model\Beer());
+                    $tableGateway = new Zend\Db\TableGateway\TableGateway('beer', $dbAdapter, null, $resultSetPrototype);
+                    $beerTableGateway = new Application\Model\BeerTableGateway($tableGateway);
+                    return $beerTableGateway;
+                },
+            ),
     ),
     'translator' => array(
-        'locale' => 'en_US',
+        'locale' => 'pt_BR',
         'translation_file_patterns' => array(
             array(
-                'type' => 'gettext',
+                'type'     => 'gettext',
                 'base_dir' => __DIR__ . '/../language',
-                'pattern' => '%s.mo',
+                'pattern'  => '%s.mo',
             ),
         ),
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController',
+            'Application\Controller\Index' => 'Application\Controller\IndexController'
         ),
     ),
     'view_manager' => array(
         'display_not_found_reason' => true,
-        'display_exceptions' => true,
-        'doctype' => 'HTML5',
-        'not_found_template' => 'error/404',
-        'exception_template' => 'error/index',
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404' => __DIR__ . '/../view/error/404.phtml',
-            'error/index' => __DIR__ . '/../view/error/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
